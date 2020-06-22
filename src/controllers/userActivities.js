61c9fcb674b9c65e79dfb101a47701bc343ef940
Giovanna <giovanna.pimenta11@gmail.com>
2020-06-22 10:20:55 -0300

    Fiz o "user activities"

---
+++ src/controllers/userActivities.js
@@ -0,0 +1,43 @@
+const Activities = require('../models/Activities');//Importando model User (schema e função)
+
+module.exports = {
+    create(req, res){
+        const activity = req.body;
+
+        Activities.newActivity(activity).then((createdActivity) => {
+            return res.send({createdActivity});
+        }).catch((error) => {
+            console.log(error);
+        });
+    },
+
+    read(req, res){
+        Activities.getAllActivities().then((allActivities) =>{
+            return res.send({allActivities});
+        }).catch((error) => {
+            console.log(error);
+        });
+    },
+
+    update(req, res){
+        const activityId = req.params.id;
+        const activityNewData = req.body;
+
+        Activities.updateActivities(activityId, activityNewData).then(() => {
+            return res.send({message: 'Usuário atualizado com sucesso'});
+        }).catch((error) => {
+            console.log(error);
+        });
+    },
+
+    delete(req, res){
+        const activityId = req.params.id;
+
+        Activities.deleteActivities(activityId).then(() => {
+            return res.send({message:`Usuário ${activityId} excluido com sucesso!`});
+        }).catch((error) => {
+            console.log(error);
+        });
+    },
+  
+};
