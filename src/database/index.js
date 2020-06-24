e10b99643a926fb9c13821befa0346f8cceb6d6b
jptrindade3 <jpr.trindade@gmail.com>
2020-06-24 00:04:14 -0300

    Melhorando comentários do código
    Pra poder me guiar melhor no futuro, adicionei comentários para me orientar no que foi feito

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');//importando a biblioteca de uso do mongo
+require('dotenv').config();//importando o .env pra uso
 
 //Setup do mongo
-require('dotenv').config(); //importando o .env pra uso
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_SERVER}/${process.env.MONGO_DATABASE}?${process.env.MONGO_OPTIONS}&w=majority`);
 mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
 mongoose.connection.once('open', () => {
--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ const routes = require('./routes');
 require('./database');//conexão com o banco de dados
 
 const  app = express();//Chamando essa função, já temos o nosso app base criado
-app.use(express.json());//Prepara nossa API pra entender informações JSON e pegar informações da URL
+app.use(express.json());//Prepara nossa API pra entender informações JSON
 app.use(routes);//Link para o arquivo de rotas
-
-//Rota de teste inicial
-app.get('/', (req, res) => {
-    res.send('Hello World! Agora, não precisa fechar o server!');
-});
 
 app.listen(3333);//Define a porta utilizada para acessar o back
